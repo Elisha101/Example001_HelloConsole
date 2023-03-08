21da88a27f039523f9d34fe99cda41cd5432aabf
Elisha <ulaigve1990@mail.ru>
2023-03-08 21:12:15 +0300

    add Example003_Same

--- Example003_Same/Program.cs
+++ Example003_Same/Program.cs
@@ -4,7 +4,9 @@ int numberB = new Random().Next(1, 10);
 Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
-
+// mew Randow().Next(min,max)
+// даст случайное целое число от min до max-1 
+// [min,max-1] или [min,max)
 
 // int numberA = 31;
 // int numberB = 2;
